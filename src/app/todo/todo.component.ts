56a5fb369f730cc386c24edbb2d03cb03267d9e9
dhanashri-bhopale <bhopaledhanashri@gmail.com>
2025-12-25 15:55:48 +0530

    first commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,61 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Itodo } from './module/todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @ViewChild('todoItem') todoItem! : ElementRef
+
+  todoArr: Array<Itodo> = [
+    {
+      todoItem: 'HTML',
+      todoId: '123'
+    },
+    {
+      todoItem: 'CSS',
+      todoId: '124'
+    },
+    {
+      todoItem: 'Javascript',
+      todoId: '125'
+    }
+  ]
+
+  onTodoAdd(){
+
+    //get TODO obj
+    let todoObj: Itodo ={
+      todoItem: this.todoItem.nativeElement.value,
+      todoId: Date.now().toString()
+    }
+    this.todoItem.nativeElement.value = ''
+    console.log(todoObj);
+
+    //push Array
+    this.todoArr.push(todoObj)
+
+    //create a new li
+
+  }
+  
+  trackById(index: number, todo: Itodo){
+    return todo.todoItem
+  }
+  
+  onRemove(id: string){
+    console.log(id)
+
+    let getIndex = this.todoArr.findIndex(t => t.todoId === id)
+    this.todoArr.splice(getIndex, 1)
+  }
+
+}
